72316356ea554471d3b92847a84fc26b933b3724
ruben <rubensousa.mieti@gmail.com>
2016-04-10 03:07:28 +0100

    Use getView() instead of member variable

--- sample/src/main/java/com/github/rubensousa/amvpsample/ui/MainPresenterImpl.java
+++ sample/src/main/java/com/github/rubensousa/amvpsample/ui/MainPresenterImpl.java
@@ -1,9 +1,5 @@
 package com.github.rubensousa.amvpsample.ui;
 
-
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-
 import com.github.rubensousa.amvp.AbstractPresenterImpl;
 
 import java.util.Random;
@@ -13,33 +9,15 @@ public class MainPresenterImpl extends AbstractPresenterImpl<MainView> implement
 
 
     private Random mRandom;
-    private MainView mView;
 
     public MainPresenterImpl() {
         mRandom = new Random();
     }
 
     @Override
-    public void onViewStateRestored(@NonNull Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-    }
-
-    @Override
-    public void attachView(MainView view) {
-        super.attachView(view);
-        mView = view;
-    }
-
-    @Override
-    public void detachView() {
-        super.detachView();
-        mView = null;
-    }
-
-    @Override
     public void generateNumber() {
-        if (mView != null) {
-            mView.showText((mRandom.nextInt(10) + 1) + "");
+        if (getView() != null) {
+            getView().showText((mRandom.nextInt(10) + 1) + "");
         }
     }
 
